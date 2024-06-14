dd3a1d34b4e763607fada816e80c698dd07e937c
izabela <izabelajilavu@gmail.com>
2024-06-15 00:18:55 +0300

    modificat path

--- src/ObstacleEnemy.cpp
+++ src/ObstacleEnemy.cpp
@@ -1,9 +1,11 @@
 #include "../headers/ObstacleEnemy.h"
 
 void ObstacleEnemy::changeDirection(Board &board, Player &player) {
-
+    if (!player.getX()) return;
+    if (!board.getWidth()) return;
 }
 
 void ObstacleEnemy::goBack(Board &board, Player &player) {
-
+    if (!player.getX()) return;
+    if (!board.getWidth()) return;
 }
